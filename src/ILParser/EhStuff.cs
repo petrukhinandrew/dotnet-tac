ec01ebe92a1f4023e4064cbd0a92a09d9416f594
Petrukhin <dartmol2300@gmail.com>
2024-09-12 12:32:44 +0300

    fix lint

--- src/ILParser/EhStuff.cs
+++ src/ILParser/EhStuff.cs
@@ -3,21 +3,25 @@ using System.Reflection.Emit;
 using Microsoft.VisualBasic;
 
 namespace Usvm.IL.Parser;
+
 public abstract record ehcType
 {
     public record Filter(int offset) : ehcType;
+
     public record Catch(Type type) : ehcType;
+
     public record Finally : ehcType;
+
     public record Fault : ehcType;
 }
 
 public class exceptionHandlingClause
 {
-    public int tryOffset;
-    public int tryLength;
-    public int handlerOffset;
-    public int handlerLength;
-    public ehcType type;
+    public readonly int tryOffset;
+    public readonly int tryLength;
+    public readonly int handlerOffset;
+    public readonly int handlerLength;
+    public readonly ehcType type;
 
     public exceptionHandlingClause(ExceptionHandlingClause c)
     {
@@ -25,11 +29,20 @@ public class exceptionHandlingClause
         ehcType ehctype;
         switch (flags)
         {
-            case ExceptionHandlingClauseOptions.Filter: ehctype = new ehcType.Filter(c.FilterOffset); break;
-            case ExceptionHandlingClauseOptions.Finally: ehctype = new ehcType.Finally(); break;
-            case ExceptionHandlingClauseOptions.Fault: ehctype = new ehcType.Fault(); break;
-            default: ehctype = new ehcType.Catch(c.CatchType ?? throw new Exception("unexpected null type")); break;
+            case ExceptionHandlingClauseOptions.Filter:
+                ehctype = new ehcType.Filter(c.FilterOffset);
+                break;
+            case ExceptionHandlingClauseOptions.Finally:
+                ehctype = new ehcType.Finally();
+                break;
+            case ExceptionHandlingClauseOptions.Fault:
+                ehctype = new ehcType.Fault();
+                break;
+            default:
+                ehctype = new ehcType.Catch(c.CatchType ?? throw new Exception("unexpected null type"));
+                break;
         }
+
         tryOffset = c.TryOffset;
         tryLength = c.TryLength;
         handlerOffset = c.HandlerOffset;
@@ -41,32 +54,30 @@ public class exceptionHandlingClause
 public abstract record rewriterEhcType
 {
     public record FilterEH(ILInstr instr) : rewriterEhcType;
+
     public record CatchEH(Type type) : rewriterEhcType;
+
     public record FinallyEH() : rewriterEhcType;
+
     public record FaultEH() : rewriterEhcType;
 }
 
-class ehClause
+class ehClause(ILInstr tryB, ILInstr tryE, ILInstr handlerB, ILInstr handlerE, rewriterEhcType type)
 {
-    public ehClause(ILInstr tryB, ILInstr tryE, ILInstr handlerB, ILInstr handlerE, rewriterEhcType type)
-    {
-        tryBegin = tryB;
-        tryEnd = tryE;
-        handlerBegin = handlerB;
-        handlerEnd = handlerE;
-        ehcType = type;
-    }
-    public ILInstr tryBegin;
-    public ILInstr tryEnd;
-    public ILInstr handlerBegin;
-    public ILInstr handlerEnd;
-    public rewriterEhcType ehcType;
+    public ILInstr tryBegin = tryB;
+    public ILInstr tryEnd = tryE;
+    public ILInstr handlerBegin = handlerB;
+    public ILInstr handlerEnd = handlerE;
+    public rewriterEhcType ehcType = type;
+
     public override string ToString()
     {
-        string extra = ehcType switch {
+        string extra = ehcType switch
+        {
             rewriterEhcType.FilterEH f => f.instr.idx.ToString(),
             _ => ""
         };
-        return string.Format("{0} {1} {2} {3} {4} {5}", ehcType.ToString(), tryBegin.idx.ToString(), tryEnd.idx.ToString(), handlerBegin.idx.ToString(), handlerEnd.idx.ToString(), extra);
+        return
+            $"{ehcType.ToString()} {tryBegin.idx.ToString()} {tryEnd.idx.ToString()} {handlerBegin.idx.ToString()} {handlerEnd.idx.ToString()} {extra}";
     }
 }
