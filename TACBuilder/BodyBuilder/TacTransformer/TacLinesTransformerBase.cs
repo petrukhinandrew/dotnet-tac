7a3ed6674dabcf708e37437d1c7fb26f9d35a7b5
Andrew Petrukhin <dartmol2300@gmail.com>
2025-05-02 16:49:18 +0300

    make TacLinesTransformerBase impl public

--- TACBuilder/BodyBuilder/TacTransformer/TacLinesTransformerBase.cs
+++ TACBuilder/BodyBuilder/TacTransformer/TacLinesTransformerBase.cs
@@ -21,16 +21,16 @@ public interface TacLinesTransformerBase<TPos>
 
 public class TacLinesTransformerIndexImpl(IlMethod method) : TacLinesTransformerBase<int>
 {
-    protected List<IlStmt>? _lines => method.Body?.Lines;
-    protected List<EhScope> _scopes => method.Scopes;
+    public List<IlStmt>? Lines => method.Body?.Lines;
+    public List<EhScope> Scopes => method.Scopes;
 
     public void InsertRange(int pos, IEnumerable<IlStmt> stmts)
     {
-        if (_lines == null) return;
+        if (Lines == null) return;
         var stmtsList = stmts.ToList();
         var shiftSize = stmtsList.Count;
 
-        foreach (var line in _lines)
+        foreach (var line in Lines)
         {
             if (line is IlBranchStmt branch && branch.Target >= pos)
             {
@@ -38,9 +38,9 @@ public class TacLinesTransformerIndexImpl(IlMethod method) : TacLinesTransformer
             }
         }
 
-        _lines.InsertRange(pos, stmtsList);
+        Lines.InsertRange(pos, stmtsList);
 
-        foreach (var scope in _scopes)
+        foreach (var scope in Scopes)
         {
             scope.tacLoc.ShiftRight(pos, shiftSize);
         }
@@ -51,10 +51,10 @@ public class TacLinesTransformerIndexImpl(IlMethod method) : TacLinesTransformer
      */
     public void RemoveRange(int begin, int end)
     {
-        if (_lines == null) return;
+        if (Lines == null) return;
         var shiftSize = end - begin + 1;
-        _lines.RemoveRange(begin, shiftSize);
-        foreach (var scope in _scopes)
+        Lines.RemoveRange(begin, shiftSize);
+        foreach (var scope in Scopes)
         {
             scope.tacLoc.ShiftLeft(begin, shiftSize);
         }
@@ -62,18 +62,18 @@ public class TacLinesTransformerIndexImpl(IlMethod method) : TacLinesTransformer
 
     public void ApplyToSlice(int pos, int length, Func<IlStmt, IlStmt> action)
     {
-        if (_lines == null) return;
+        if (Lines == null) return;
         for (int i = pos; i < pos + length; i++)
         {
-            _lines[i] = action(_lines[i]);
+            Lines[i] = action(Lines[i]);
         }
     }
 
     public List<IlStmt> SliceCopy(int pos, int length)
     {
-        if (_lines == null) return [];
+        if (Lines == null) return [];
         IlStmt[] copy = new IlStmt[length];
-        _lines.CopyTo(pos, copy, 0, length);
+        Lines.CopyTo(pos, copy, 0, length);
         for (var i = 0; i < length; i++)
             if (copy[i] is IlBranchStmt branch)
                 copy[i] = branch.Copy();
@@ -82,7 +82,7 @@ public class TacLinesTransformerIndexImpl(IlMethod method) : TacLinesTransformer
 
     public void DuplicateSlice(int slicePos, int sliceLength, int dst)
     {
-        if (_lines == null) return;
+        if (Lines == null) return;
         Debug.Assert(dst > slicePos);
         var slice = SliceCopy(slicePos, sliceLength);
         InsertRange(dst, slice);
@@ -95,8 +95,8 @@ public class TacLinesTransformerIndexImpl(IlMethod method) : TacLinesTransformer
             copy.Target = copy.Target + dst - slicePos;
             return copy;
         });
-        var duplicates = _scopes.Where(s => s.IsInSegment(slicePos, slicePos + sliceLength))
+        var duplicates = Scopes.Where(s => s.IsInSegment(slicePos, slicePos + sliceLength))
             .Select(s => s.ShiftedRightAt(slicePos - s.tacLoc.tb + dst)).ToList();
-        _scopes.AddRange(duplicates);
+        Scopes.AddRange(duplicates);
     }
 }
